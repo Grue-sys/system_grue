97336edfab883ebc0f3f74741be97bbc07a11628
jakub <jakub.sz@centrum.cz>
2024-09-23 09:21:48 +0200

    feature - model > product

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+
+class ProductBranch(Model):
+    name = CharField(max_length=20, null=False, blank=False, unique=True)
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"ProductBranch(name={self.name})"
+
+
+class ProductType(Model):
+    name = CharField(max_length=25, null=False, blank=False)
+    branch_type = ManyToManyField(ProductBranch, blank=True, related_name="product_types")
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"ProductType(name={self.name})"
+
+
+class ProductDetail(Model):
+    name = CharField(max_length=25, null=False, blank=False)
+    product_type = ManyToManyField(ProductType, blank=True, related_name="products")
+    detailed_info = TextField(null=True, blank=True)
+    more_info = TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"Product(name={self.name})"
